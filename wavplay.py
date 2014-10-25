3726912faebc1643d3939e5cbc0f8957e1e40833
Mike Burr <meburr@gmail.com>
2014-10-25 09:17:47 +0000

    make things more reusable

--- wavplay.py
+++ wavplay.py
@@ -1,11 +1,6 @@
 import os
 from wave import open as waveOpen
 from ossaudiodev import open as ossOpen
-
-s = waveOpen(os.path.join(os.path.dirname(__file__), '1.wav'), 'rb')
-nc, sw, fr, nf, comptype, compname = s.getparams()
-dsp = ossOpen('/dev/dsp','w')
-
 try:
     from ossaudiodev import AFMT_S16_NE
 except ImportError:
@@ -14,8 +9,18 @@ except ImportError:
     else:
         AFMT_S16_NE = ossaudiodev.AFMT_S16_BE
 
-dsp.setparameters(AFMT_S16_NE, nc, fr)
-data = s.readframes(nf)
-s.close()
-dsp.write(data)
-dsp.close()
+def wav_to_dsp(wav_path):
+
+    try:
+        s = waveOpen(wav_path, 'rb')
+        dsp = ossOpen('/dev/dsp','w')
+        nc, sw, fr, nf, comptype, compname = s.getparams()
+        dsp.setparameters(AFMT_S16_NE, nc, fr)
+        data = s.readframes(nf)
+        dsp.write(data)
+    finally:
+        for obj in s, dsp:
+            try:
+                getattr(obj, 'close')()
+            except Exception:
+                pass
