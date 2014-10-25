462ff222322002c5ccfc2495931f5f9dd681b9c4
Mike Burr <meburr@gmail.com>
2014-10-25 10:33:12 +0000

    beautify output

--- dsm.py
+++ dsm.py
@@ -20,17 +20,33 @@ try:
             for x in range(16):
                 bytes.append(ser.read(1))
 
-            if bytes[-1] != '\r':  # throw away
+            if bytes[-1] != '\r':  # throw away frame that doesn't end with \r
                 continue
 
             s = ''
             for i, b in enumerate(bytes):
-                s += '{0}: {1}\n'.format(i, repr(b))
+                label = i
+                if label in (0,):
+                    label = str(label) + '\trudder'
+                elif label in (11, 12):
+                    label = str(label) + '\televator'
+                elif label in (13, 14):
+                    label = str(label) + '\tgear'
+                elif label in (3, 4):
+                    label = str(label) + '\tthrottle'
+                elif label in (9, 10):
+                    label = str(label) + '\tflaps'
+                elif label in (7, 8):
+                    label = str(label) + '\taileron'
+                else:
+                    label = str(label) + '\t???????'
+                s += '{}:\t\t{:0=8b}\n'.format(label, ord(b))
             stdscr.addstr(0, 0, s)
             stdscr.refresh()
 
 except KeyboardInterrupt:
     pass
+
 finally:
     curses.endwin()
 
