9800d006db0282e258b6cdb85da6dd9d89b8da02
Mike Burr <meburr@gmail.com>
2014-10-31 10:10:59 +0000

    separate module for wsgi app

---
+++ myrpi/raspiweb.py
@@ -0,0 +1,14 @@
+from wsgiref.util import setup_testing_defaults
+
+def raspiweb(environ, start_response):
+    setup_testing_defaults(environ)
+
+    status = '200 OK'
+    headers = [('Content-type', 'text/plain')]
+
+    start_response(status, headers)
+
+    ret = ["%s: %s\n" % (key, value)
+           for key, value in environ.iteritems()]
+    return ret
+
