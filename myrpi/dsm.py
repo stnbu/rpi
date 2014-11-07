30fdd04222696b9c09573b2f64d6d7b883f5af86
Mike Burr <meburr@gmail.com>
2014-11-07 03:25:55 +0000

    new, working dsm.py

--- myrpi/dsm.py
+++ myrpi/dsm.py
@@ -1,54 +1,53 @@
 import serial
-import sys
-import os
 import curses
 
+def format_channel_data(bytes):
+    for index, byte in enumerate(bytes):
+        if index % 2:  # if an odd member
+            channel_id = last & 0b11111100
+            last = last & 0b00000011  # mask the first 6 bits
+            last = last << 8
+            position = byte | last  # "append" byte to the last byte
+            yield channel_id, position
+        last = byte
 
-byte = None
 
 def do_dsm():
-    try:
-        with serial.Serial(port='/dev/ttyAMA0', baudrate=115200, bytesize=8,
-                           parity='N', stopbits=1, timeout=10) as ser:
-            while byte != '\r':
-                byte = ser.read(1)
-                continue
 
-            stdscr = curses.initscr()
-            # next byte starts thing
-            while True:
+    with serial.Serial(port='/dev/ttyUSB0', baudrate=115200, bytesize=8, parity='N', stopbits=1) as ser:
+
+        byte = None
+
+        while byte != '\r':  # read, discard first frame
+            byte = ser.read(1)
+            continue
+
+        stdscr = curses.initscr()
+
+        while True:
+            try:
                 bytes = []
                 for x in range(16):
                     bytes.append(ser.read(1))
 
-                if bytes[-1] != '\r':  # throw away frame that doesn't end with \r
-                    continue
+                bytes = bytes[-1:] + bytes[:-1]
+                bytes = map(ord, bytes)
+
+                channel_data = format_channel_data(bytes)
 
                 s = ''
-                for i, b in enumerate(bytes):
-                    label = i
-                    if label in (0,):
-                        label = str(label) + '\trudder'
-                    elif label in (11, 12):
-                        label = str(label) + '\televator'
-                    elif label in (13, 14):
-                        label = str(label) + '\tgear'
-                    elif label in (3, 4):
-                        label = str(label) + '\tthrottle'
-                    elif label in (9, 10):
-                        label = str(label) + '\tflaps'
-                    elif label in (7, 8):
-                        label = str(label) + '\taileron'
-                    else:
-                        label = str(label) + '\t???????'
-                    s += '{}:\t\t{:0=8b}\n'.format(label, ord(b))
+                for channel_id, position in channel_data:
+                    s += 'c: {0}\t{1}\n'.format(channel_id, position)
+                s += '\n'
+                s += '=' * 120
+
                 stdscr.addstr(0, 0, s)
                 stdscr.refresh()
 
-    except KeyboardInterrupt:
-        pass
+            except KeyboardInterrupt:
+                curses.endwin()
+                break
 
-    finally:
         curses.endwin()
 
 if __name__ == '__main__':
