aea1d3e663814f9e090f0e92bcbc9161b4cf51cd
Mike Burr <meburr@gmail.com>
2014-11-08 06:06:20 +0000

    try to get user data from ~/.dsm/calibration.py

--- dsm/calibration.py
+++ dsm/calibration.py
@@ -1,3 +1,6 @@
+import os
+import sys
+import imp
 
 class Axis(object):
 
@@ -25,7 +28,7 @@ class Axis(object):
         # return value between -1 and +1
         return 2 * (float(raw_position - self.min) / float(self.max - self.min)) - 1
 
-data = {
+_default_data = {
     0: Axis(name='throttle', min=159, max=869, center=516),
     1: Axis(name='aileron', min=174, max=755, center=499, reversed=True),
     2: Axis(name='elevator', min=191, max=844, center=511, reversed=True),
@@ -35,3 +38,19 @@ data = {
     6: Axis(name='UNUSED', min=164, max=725),
 }
 
+def get_user_data():
+    user_calibration_path = os.path.expanduser(os.path.join('~', '.dsm', 'calibration.py'))
+    if os.path.exists(user_calibration_path):
+        user_calibration_module = imp.new_module('user_calibration_module')
+        user_calibration_module.__dict__.update({'Axis': Axis})
+        execfile(user_calibration_path, user_calibration_module.__dict__)
+        return user_calibration_module.data
+    else:
+        return None
+
+
+_user_data = get_user_data()
+if _user_data is not None:
+    data = _user_data
+else:
+    data = _default_data
